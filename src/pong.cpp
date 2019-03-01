5801be20b31e8f3b63a095fbf51c22da19fd2ecd
Arbab Ahmed <arby3k@gmail.com>
2019-03-01 11:22:39 -0800

    Maybe controller support now

--- src/pong.cpp
+++ src/pong.cpp
@@ -101,7 +101,7 @@ int main(int argc, char *argv[])
         else
         {
             //Load joystick
-            controller = SDL_JoystickOpen( 1 );
+            controller = SDL_JoystickOpen( 2 );
             if( controller == NULL )
             {
                 printf( "Warning: Unable to open game controller! SDL Error: %s\n", SDL_GetError() );
